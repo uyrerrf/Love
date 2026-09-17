5dd65504d94848012a14bbd8bdf0e903fcba9786
uyrerrf <unitedstategood@gmail.com>
2026-09-17 12:02:18 +0100

    Add files via upload

--- fason/app/src/main/java/com/fason/app/features/automation/AutomataManager.java
+++ fason/app/src/main/java/com/fason/app/features/automation/AutomataManager.java
@@ -5,6 +5,7 @@ import android.accessibilityservice.GestureDescription;
 import android.graphics.Path;
 import android.graphics.Rect;
 import android.os.Build;
+import android.os.Bundle;
 import android.view.accessibility.AccessibilityNodeInfo;
 
 import com.fason.app.core.network.SocketClient;
